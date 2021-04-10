85a9fab08e9f79b1d171bb3301eaa550d5a24a59
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-04-10 16:47:33 +0300

    Add js functions

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 
 function getAkanName () {
-  let yearOfBirth = document.getElementById("year").value;
-  let monthOfBirth = Number(document.getElementById("month").value);
-  let dayOfBirth = Number(document.getElementById("day").value);
+  let yearOfBirth = document.getElementById("year-input").value;
+  let monthOfBirth = Number(document.getElementById("month-input").value);
+  let dayOfBirth = Number(document.getElementById("day-input").value);
   let genders = document.getElementsByName("gender");
 
   function getGender () {
